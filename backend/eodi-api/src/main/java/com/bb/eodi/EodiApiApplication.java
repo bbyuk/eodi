3d0d87d3d6fa4ab6dc8344fa5ca3a5925940f5d0
hyuk <k941026h@naver.com>
2025-09-01 22:51:33 +0900

    refactor(eodi-api): 애플리케이션 명 변경

--- backend/eodi-api/src/main/java/com/bb/eodi/EodiApiApplication.java
+++ backend/eodi-api/src/main/java/com/bb/eodi/EodiApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EodiApplication {
+public class EodiApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EodiApplication.class, args);
+		SpringApplication.run(EodiApiApplication.class, args);
 	}
 
 }
--- backend/eodi-api/src/test/java/com/bb/eodi/EodiApiApplicationTests.java
+++ backend/eodi-api/src/test/java/com/bb/eodi/EodiApiApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EodiApplicationTests {
+class EodiApiApplicationTests {
 
 	@Test
 	void contextLoads() {
