97c3913903c40fa4d893854b7b38e68b068aae62
hyuk <k941026h@naver.com>
2025-09-01 22:02:59 +0900

    feature(eodi-batch): 배치 프로젝트 추가

---
+++ backend/eodi-batch/src/main/java/com/bb/eodi_batch/EodiBatchApplication.java
@@ -0,0 +1,13 @@
+package com.bb.eodi_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodiBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodiBatchApplication.class, args);
+	}
+
+}
