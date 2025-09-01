a8b7d56aaf5f7e41e3d028a0cff9ecb49239aaae
hyuk <k941026h@naver.com>
2025-09-01 20:27:09 +0900

    feat(eodi-api): eodi-api 프로젝트 시작

---
+++ backend/eodi-api/src/main/java/com/bb/eodi/EodiApplication.java
@@ -0,0 +1,13 @@
+package com.bb.eodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodiApplication.class, args);
+	}
+
+}
---
+++ backend/eodi-api/src/test/java/com/bb/eodi/EodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bb.eodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
