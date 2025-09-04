93ed2c625ec0fe053c050947e0ae73479754b5d6
bbyuk <k941026h@naver.com>
2025-09-04 13:28:04 +0900

    refactor(eodi-batch): 법정동코드 테이블 구조 변경에 따른 엔티티 및 repository 변경

--- backend/eodi-batch/src/main/java/com/bb/eodi/domain/legaldong/entity/LegalDong.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/domain/legaldong/entity/LegalDong.java
@@ -4,14 +4,21 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class LegalDong {
     @Id
     @GeneratedValue
     @Column(name = "id")
-    private Long id;
+    protected Long id;
 
     @Column(name = "name", length = 50)
-    private String name;
+    protected String name;
+
+    public LegalDong(String name) {
+        this.name = name;
+    }
 }
