08cb88b0dae089d089d9fd703fd5e440b61b88d7
bbyuk <k941026h@naver.com>
2025-09-04 13:30:36 +0900

    chore(eodi-batch): 법정동코드 적재배치 processor 로직 구현 / LegalDong entity 및 repo 변경

--- backend/eodi-batch/src/main/java/com/bb/eodi/domain/legaldong/entity/LegalDong.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/domain/legaldong/entity/LegalDong.java
@@ -1,24 +1,57 @@
 package com.bb.eodi.domain.legaldong.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+/**
+ * 법정동코드
+ */
+@Getter
 @Entity
+@Table(name = "legal_dong")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public abstract class LegalDong {
+public class LegalDong {
     @Id
     @GeneratedValue
     @Column(name = "id")
-    protected Long id;
+    private Long id;
 
-    @Column(name = "name", length = 50)
-    protected String name;
+    @Column(name = "code", length = 3, nullable = false, unique = true)
+    private String code;
 
-    public LegalDong(String name) {
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "parent_id")
+    private Long parentId;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updatedAt", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public LegalDong(String code, String name, boolean isActive) {
+        this.code = code;
         this.name = name;
+        this.isActive = isActive;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public LegalDong(String code, String name, Long parentId, boolean isActive) {
+        this.code = code;
+        this.name = name;
+        this.parentId = parentId;
+        this.isActive = isActive;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
     }
 }
---
+++ backend/eodi-batch/src/main/java/com/bb/eodi/domain/legaldong/repository/LegalDongRepository.java
@@ -0,0 +1,7 @@
+package com.bb.eodi.domain.legaldong.repository;
+
+/**
+ * 법정동코드 repository
+ */
+public interface LegalDongRepository {
+}
