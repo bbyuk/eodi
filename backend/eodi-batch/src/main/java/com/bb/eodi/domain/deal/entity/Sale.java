21958cc4cf1ceddcbeab0a13bfaba61faa3280cc
bbyuk <k941026h@naver.com>
2025-09-05 15:35:50 +0900

    build(eodi-batch): spring-data-jpa 의존성 제거 작업

--- backend/eodi-batch/src/main/java/com/bb/eodi/domain/deal/entity/Sale.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/domain/deal/entity/Sale.java
@@ -2,27 +2,23 @@ package com.bb.eodi.domain.deal.entity;
 
 import com.bb.eodi.domain.deal.type.HousingType;
 import com.bb.eodi.domain.deal.type.TradeMethodType;
-import com.bb.eodi.domain.deal.vo.Region;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
+import javax.swing.plaf.synth.Region;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * 부동산 매매 데이터 엔티티
  */
 @Getter
-@Table(name = "real_esate_sale")
-@Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Sale {
 
     @Builder
-    public Sale(Region region, String roadName, LocalDate contractDate, int price, TradeMethodType tradeMethodType, LocalDate cancelDate, int buildYear, BigDecimal netLeasableArea, BigDecimal landArea, String buyer, String seller, HousingType housingType, LocalDate registrationDate, String buildingName, Integer dong, Integer floor, BigDecimal totalFloorArea, BigDecimal siteArea) {
+    public Sale(Region region, String roadName, LocalDate contractDate, int price, TradeMethodType tradeMethodType, LocalDate cancelDate, int buildYear, BigDecimal netLeasableArea, BigDecimal landArea, String buyer, String seller, HousingType housingType, LocalDate registrationDate, String targetName, String dong, Integer floor, BigDecimal totalFloorArea, BigDecimal siteArea) {
         this.region = region;
         this.roadName = roadName;
         this.contractDate = contractDate;
@@ -36,86 +32,70 @@ public class Sale {
         this.seller = seller;
         this.housingType = housingType;
         this.registrationDate = registrationDate;
-        this.buildingName = buildingName;
+        this.targetName = targetName;
         this.dong = dong;
         this.floor = floor;
         this.totalFloorArea = totalFloorArea;
-        this.siteArea = siteArea;
     }
 
-    @Id @GeneratedValue
-    @Column(name = "id")
+    @Id
     private Long id;
 
     // 행정구역
-    @Embedded
     private Region region;
 
     // 도로명
-    @Column(name = "road_name")
     private String roadName;
 
     // 계약일
-    @Column(name = "contract_date")
     private LocalDate contractDate;
 
     // 거래금액 (만원)
-    @Column(name = "price")
     private int price;
 
     // 거래 방법
-    @Column(name = "trade_method_type")
     private TradeMethodType tradeMethodType;
 
     // 해제사유 발생일
-    @Column(name = "cancel_date")
     private LocalDate cancelDate;
 
     // 건축년도
-    @Column(name = "build_year")
     private int buildYear;
 
     // 전용면적
-    @Column(name = "net_leasable_area", precision = 10, scale = 4)
     private BigDecimal netLeasableArea;
 
     // 대지면적
-    @Column(name = "land_area", precision = 10, scale = 4)
     private BigDecimal landArea;
 
+    // 연면적
+    private BigDecimal totalFloorArea;
+
     // 매수자
-    @Column(name = "buyer")
     private String buyer;
 
     // 매도자
-    @Column(name = "seller")
     private String seller;
     
     // 주택 유형
-    @Column(name = "housing_type")
     private HousingType housingType;
 
     // 등기일자
-    @Column(name = "registration_date")
     private LocalDate registrationDate;
 
-    // 건물 || 단지명
-    @Column(name = "building_name")
-    private String buildingName;
+    // 대상명
+    private String targetName;
 
     // 건물 동
-    @Column(name = "dong")
-    private Integer dong;
+    private String dong;
 
     // 층
-    @Column(name = "floor")
     private Integer floor;
 
-    // 연면적
-    @Column(name = "total_floor_area", precision = 10, scale = 4)
-    private BigDecimal totalFloorArea;
+    // 생성일시
+    private LocalDateTime createdAt;
 
-    // 대지면적
-    @Column(name = "site_area", precision = 10, scale = 4)
-    private BigDecimal siteArea;
+    // 수정일시
+    private LocalDateTime updatedAt;
+
 }
--- backend/eodi-batch/src/main/java/com/bb/eodi/domain/legaldong/entity/LegalDong.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/domain/legaldong/entity/LegalDong.java
@@ -1,7 +1,7 @@
 package com.bb.eodi.domain.legaldong.entity;
 
-import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,41 +11,31 @@ import java.time.LocalDateTime;
  * 법정동코드
  */
 @Getter
-@Entity
-@Table(name = "legal_dong")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LegalDong {
-    @Id
-    @GeneratedValue
-    @Column(name = "id")
+
+    // 법정동 ID
     private Long id;
 
-    @Column(name = "code", length = 3, nullable = false, unique = true)
+    // 법정동 코드
     private String code;
 
-    @Column(name = "name", length = 50, nullable = false)
+    // 법정동 명
     private String name;
 
-    @Column(name = "parent_id")
+    // 상위 법정동 ID
     private Long parentId;
 
-    @Column(name = "is_active", nullable = false)
+    // 활성여부
     private boolean isActive;
 
-    @Column(name = "created_at", nullable = false)
+    // 생성일시
     private LocalDateTime createdAt;
 
-    @Column(name = "updatedAt", nullable = false)
+    // 변경 일시
     private LocalDateTime updatedAt;
 
-    public LegalDong(String code, String name, boolean isActive) {
-        this.code = code;
-        this.name = name;
-        this.isActive = isActive;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
-    }
-
+    @Builder
     public LegalDong(String code, String name, Long parentId, boolean isActive) {
         this.code = code;
         this.name = name;
