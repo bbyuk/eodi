98b2ed1d8f61ec0a556efdaeb97fdf067df914d7
bbyuk <k941026h@naver.com>
2025-09-05 14:47:29 +0900

    feat(eodi-batch): HousingType 코드 추가

--- backend/eodi-batch/src/main/java/com/bb/eodi/domain/deal/type/HousingType.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/domain/deal/type/HousingType.java
@@ -5,25 +5,26 @@ package com.bb.eodi.domain.deal.type;
  */
 public enum HousingType implements Type {
     // 아파트
-    APT("A", "아파트"),
+    APT("AP", "아파트"),
 
     // 연립/다세대
     MULTI_HOUSEHOLD_HOUSE("MH", "연립/다세대 주택"),
 
     // 단독
-    DETACHED_HOUSE("D", "단독 주택"),
+    DETACHED_HOUSE("DT", "단독 주택"),
 
     // 다가구주택
     MULTI_UNIT_HOUSE("MU", "다가구 주택"),
 
     // 오피스텔
-    OFFICETEL("O", "오피스텔"),
+    OFFICETEL("OF", "오피스텔"),
 
     // 분양권
     PRE_SALE_RIGHT("PR", "분양권"),
     
     // 입주권
-    OCCUPY_RIGHT("OR", "입주권");
+    OCCUPY_RIGHT("OR", "입주권"),
+    OTHER("O", "기타");
 
     private final String code;
     private final String description;
