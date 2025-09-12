1cafe4b6a41710ce9c95e237de4c1176800ec3f0
bbyuk <k941026h@naver.com>
2025-09-12 09:13:52 +0900

    refactor(eodi-batch): 실거래가 데이터 API Client serviceKey 주입 공통화

--- backend/eodi-batch/src/main/java/com/bb/eodi/infrastructure/http/deal/DealDataApiClient.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/infrastructure/http/deal/DealDataApiClient.java
@@ -1,6 +1,5 @@
 package com.bb.eodi.infrastructure.http.deal;
 
-import com.bb.eodi.infrastructure.api.govdata.GovernmentDataApiProperties;
 import com.bb.eodi.port.out.deal.DealDataPort;
 import com.bb.eodi.port.out.deal.dto.*;
 import lombok.RequiredArgsConstructor;
@@ -14,50 +13,49 @@ import org.springframework.stereotype.Component;
 public class DealDataApiClient implements DealDataPort {
 
     private final DealDataApi dealDataApi;
-    private final GovernmentDataApiProperties properties;
 
     @Override
     public DealDataResponse<ApartmentSellDataItem> getApartmentSellData(DealDataQuery query) {
-        return dealDataApi.getApartmentSellData(query.legalDongCode(), query.dealDate(), properties.key());
+        return dealDataApi.getApartmentSellData(query.legalDongCode(), query.dealDate());
     }
 
     @Override
     public DealDataResponse<ApartmentLeaseDataItem> getApartmentLeaseData(DealDataQuery query) {
-        return dealDataApi.getApartmentLeaseData(query.legalDongCode(), query.dealDate(), properties.key());
+        return dealDataApi.getApartmentLeaseData(query.legalDongCode(), query.dealDate());
     }
 
     @Override
     public DealDataResponse<ApartmentPreSaleRightSaleDataItem> getApartmentPreSaleRightSaleData(DealDataQuery query) {
-        return dealDataApi.getApartmentPreSaleRightSaleData(query.legalDongCode(), query.dealDate(), properties.key());
+        return dealDataApi.getApartmentPreSaleRightSaleData(query.legalDongCode(), query.dealDate());
     }
 
     @Override
     public DealDataResponse<MultiHouseholdSellDataItem> getMultiHouseholdSellData(DealDataQuery query) {
-        return dealDataApi.getMultiHouseholdSellData(query.legalDongCode(), query.dealDate(), properties.key());
+        return dealDataApi.getMultiHouseholdSellData(query.legalDongCode(), query.dealDate());
     }
 
     @Override
     public DealDataResponse<MultiHouseholdLeaseDataItem> getMultiHouseholdLeaseData(DealDataQuery query) {
-        return dealDataApi.getMultiHouseholdLeaseData(query.legalDongCode(), query.dealDate(), properties.key());
+        return dealDataApi.getMultiHouseholdLeaseData(query.legalDongCode(), query.dealDate());
     }
 
     @Override
     public DealDataResponse<MultiUnitDetachedSellDataItem> getMultiUnitDetachedSellData(DealDataQuery query) {
-        return dealDataApi.getMultiUnitDetachedSellData(query.legalDongCode(), query.dealDate(), properties.key());
+        return dealDataApi.getMultiUnitDetachedSellData(query.legalDongCode(), query.dealDate());
     }
 
     @Override
     public DealDataResponse<MultiUnitDetachedLeaseDataItem> getMultiUnitDetachedLeaseData(DealDataQuery query) {
-        return dealDataApi.getMultiUnitDetachedLeaseData(query.legalDongCode(), query.dealDate(), properties.key());
+        return dealDataApi.getMultiUnitDetachedLeaseData(query.legalDongCode(), query.dealDate());
     }
 
     @Override
     public DealDataResponse<OfficetelSellDataItem> getOfficetelSellData(DealDataQuery query) {
-        return dealDataApi.getOfficetelSellData(query.legalDongCode(), query.dealDate(), properties.key());
+        return dealDataApi.getOfficetelSellData(query.legalDongCode(), query.dealDate());
     }
 
     @Override
     public DealDataResponse<OfficetelLeaseDataItem> getOfficetelLeaseData(DealDataQuery query) {
-        return dealDataApi.getOfficetelLeaseData(query.legalDongCode(), query.dealDate(), properties.key());
+        return dealDataApi.getOfficetelLeaseData(query.legalDongCode(), query.dealDate());
     }
 }
