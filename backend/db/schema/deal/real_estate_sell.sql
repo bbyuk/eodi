07d3c1f474c4377768c5e0403b1052d5dbafccf6
bbyuk <k941026h@naver.com>
2025-09-15 14:58:23 +0900

    db: sell -> real_estate_sell 테이블 명 변경

--- backend/db/schema/deal/real_estate_sell.sql
+++ backend/db/schema/deal/real_estate_sell.sql
@@ -2,11 +2,11 @@
 SET NAMES utf8mb4;
 SET time_zone = '+09:00';
 
-DROP TABLE IF EXISTS sell;
+DROP TABLE IF EXISTS real_estate_sell;
 
 -- -----------------------------------------------------------------------------------------
 -- create
-CREATE TABLE sell
+CREATE TABLE real_estate_sell
 (
     id                      BIGINT  AUTO_INCREMENT PRIMARY KEY COMMENT '매매 ID',
     region_id               BIGINT  NOT NULL COMMENT '대상지역 법정동 ID',
@@ -20,7 +20,7 @@ CREATE TABLE sell
     land_area               DECIMAL(10, 4) COMMENT '대지면적',
     total_floor_area        DECIMAL(10, 4) COMMENT '연면적',
     buyer                   VARCHAR(50) COMMENT '매수자',
-    seller                  VARCHAR(50) COMMENT '매도자',
+    real_estate_seller                  VARCHAR(50) COMMENT '매도자',
     housing_type            ENUM('AP', 'MH', 'DT', 'MU', 'OF', 'PR', 'DR', 'O') NOT NULL DEFAULT 'O' COMMENT '주택유형',
     date_of_registration    DATE    COMMENT '등기일자',
     target_name             VARCHAR(100) NOT NULL COMMENT '대상명',
@@ -32,17 +32,17 @@ CREATE TABLE sell
 ) COMMENT = '매매';
 -- -----------------------------------------------------------------------------------------
 -- constraint
-ALTER TABLE sell
-    ADD CONSTRAINT fk_sell__region_id
+ALTER TABLE real_estate_sell
+    ADD CONSTRAINT fk_real_estate_sell__region_id
         FOREIGN KEY (region_id)
             REFERENCES legal_dong(id)
             ON UPDATE RESTRICT
             ON DELETE RESTRICT;
 -- -----------------------------------------------------------------------------------------
 -- index
-CREATE INDEX idx_sell__ri ON sell(region_id);
-CREATE INDEX idx_sell__tn ON sell(target_name);
-CREATE INDEX idx_sell__cd ON sell(contract_date);
-CREATE INDEX idx_sell__ht ON sell(housing_type);
-CREATE INDEX idx_sell__p ON sell(price);
-CREATE INDEX idx_sell__nla ON sell(net_leasable_area);
+CREATE INDEX idx_real_estate_sell__ri ON real_estate_sell(region_id);
+CREATE INDEX idx_real_estate_sell__tn ON real_estate_sell(target_name);
+CREATE INDEX idx_real_estate_sell__cd ON real_estate_sell(contract_date);
+CREATE INDEX idx_real_estate_sell__ht ON real_estate_sell(housing_type);
+CREATE INDEX idx_real_estate_sell__p ON real_estate_sell(price);
+CREATE INDEX idx_real_estate_sell__nla ON real_estate_sell(net_leasable_area);
--- backend/db/schema/integration/schema.sql
+++ backend/db/schema/integration/schema.sql
@@ -4,7 +4,7 @@ SET time_zone = '+09:00';
 
 /* --------------------------------------------------------------------------------------- */
 -- drop table
-DROP TABLE IF EXISTS sell;
+DROP TABLE IF EXISTS real_estate_sell;
 DROP TABLE IF EXISTS lease;
 DROP TABLE IF EXISTS legal_dong;
 
@@ -54,10 +54,10 @@ CREATE INDEX idx_ld__code              ON legal_dong(code);
 -- -----------------------------------------------------------------------------------------
 
 
--- sell
+-- real_estate_sell
 -- -----------------------------------------------------------------------------------------
 -- create
-CREATE TABLE sell
+CREATE TABLE real_estate_sell
 (
     id                      BIGINT  AUTO_INCREMENT PRIMARY KEY COMMENT '매매 ID',
     region_id               BIGINT  NOT NULL COMMENT '대상지역 법정동 ID',
@@ -71,7 +71,7 @@ CREATE TABLE sell
     land_area               DECIMAL(10, 4) COMMENT '대지면적',
     total_floor_area        DECIMAL(10, 4) COMMENT '연면적',
     buyer                   VARCHAR(50) COMMENT '매수자',
-    seller                  VARCHAR(50) COMMENT '매도자',
+    real_estate_seller                  VARCHAR(50) COMMENT '매도자',
     housing_type            ENUM('AP', 'MH', 'DT', 'MU', 'OF', 'PR', 'DR', 'O') NOT NULL DEFAULT 'O' COMMENT '주택유형',
     date_of_registration    DATE    COMMENT '등기일자',
     target_name             VARCHAR(100) NOT NULL COMMENT '대상명',
@@ -83,20 +83,20 @@ CREATE TABLE sell
 ) COMMENT = '매매';
 -- -----------------------------------------------------------------------------------------
 -- constraint
-ALTER TABLE sell
-    ADD CONSTRAINT fk_sell__region_id
+ALTER TABLE real_estate_sell
+    ADD CONSTRAINT fk_real_estate_sell__region_id
         FOREIGN KEY (region_id)
             REFERENCES legal_dong(id)
             ON UPDATE RESTRICT
             ON DELETE RESTRICT;
 -- -----------------------------------------------------------------------------------------
 -- index
-CREATE INDEX idx_sell__ri ON sell(region_id);
-CREATE INDEX idx_sell__tn ON sell(target_name);
-CREATE INDEX idx_sell__cd ON sell(contract_date);
-CREATE INDEX idx_sell__ht ON sell(housing_type);
-CREATE INDEX idx_sell__p ON sell(price);
-CREATE INDEX idx_sell__nla ON sell(net_leasable_area);
+CREATE INDEX idx_real_estate_sell__ri ON real_estate_sell(region_id);
+CREATE INDEX idx_real_estate_sell__tn ON real_estate_sell(target_name);
+CREATE INDEX idx_real_estate_sell__cd ON real_estate_sell(contract_date);
+CREATE INDEX idx_real_estate_sell__ht ON real_estate_sell(housing_type);
+CREATE INDEX idx_real_estate_sell__p ON real_estate_sell(price);
+CREATE INDEX idx_real_estate_sell__nla ON real_estate_sell(net_leasable_area);
 -- -----------------------------------------------------------------------------------------
 
 -- lease
