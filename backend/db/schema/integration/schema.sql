80d95e48533e985144323cefb780d2fd7ad71ff8
bbyuk <k941026h@naver.com>
2025-09-19 16:49:38 +0900

    db: real_estate_lease ddl 추가 / ddl 오류 수정

--- backend/db/schema/integration/schema.sql
+++ backend/db/schema/integration/schema.sql
@@ -4,8 +4,8 @@ SET time_zone = '+09:00';
 
 /* --------------------------------------------------------------------------------------- */
 -- drop table
+DROP TABLE IF EXISTS real_estate_lease;
 DROP TABLE IF EXISTS real_estate_sell;
-DROP TABLE IF EXISTS lease;
 DROP TABLE IF EXISTS legal_dong;
 
 DROP TABLE IF EXISTS BATCH_STEP_EXECUTION_CONTEXT;
@@ -59,7 +59,7 @@ CREATE INDEX idx_ld__code              ON legal_dong(code);
 -- create
 CREATE TABLE real_estate_sell
 (
-    id                      BIGINT  AUTO_INCREMENT PRIMARY KEY COMMENT '매매 ID',
+    id                      BIGINT  AUTO_INCREMENT PRIMARY KEY COMMENT '매매 실거래가 ID',
     region_id               BIGINT  NOT NULL COMMENT '대상지역 법정동 ID',
     legal_dong_name         VARCHAR(50) COMMENT '법정동 명',
     contract_date           DATE    COMMENT '계약일',
@@ -67,20 +67,20 @@ CREATE TABLE real_estate_sell
     trade_method_type       ENUM('D', 'A', 'O') NOT NULL DEFAULT 'O' COMMENT '거래방법',
     cancel_date             DATE    COMMENT '해제사유 발생일',
     build_year              INTEGER COMMENT '건축년도',
-    net_leasable_area       DECIMAL(10, 4) NOT NULL COMMENT '전용면적',
+    net_leasable_area       DECIMAL(10, 4) COMMENT '전용면적',
     land_area               DECIMAL(10, 4) COMMENT '대지면적',
     total_floor_area        DECIMAL(10, 4) COMMENT '연면적',
     buyer                   VARCHAR(50) COMMENT '매수자',
     seller                  VARCHAR(50) COMMENT '매도자',
     housing_type            ENUM('AP', 'MH', 'DT', 'MU', 'OF', 'PR', 'OR', 'O') NOT NULL DEFAULT 'O' COMMENT '주택유형',
     date_of_registration    DATE    COMMENT '등기일자',
-    target_name             VARCHAR(100) NOT NULL COMMENT '대상명',
+    target_name             VARCHAR(100) COMMENT '대상명',
     building_dong           VARCHAR(50) COMMENT '건물 동',
     floor                   INTEGER COMMENT '층',
-    is_land_lease           TINYINT(1) NOT NULL DEFAULT 0 COMMENT '활성 여부',
+    is_land_lease           TINYINT(1) NOT NULL DEFAULT 0 COMMENT '토지임대부 여부',
     created_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
     updated_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시'
-) COMMENT = '매매';
+) COMMENT = '부동산 매매 실거래가';
 -- -----------------------------------------------------------------------------------------
 -- constraint
 ALTER TABLE real_estate_sell
@@ -92,20 +92,42 @@ ALTER TABLE real_estate_sell
 -- -----------------------------------------------------------------------------------------
 -- index
 CREATE INDEX idx_real_estate_sell__ri ON real_estate_sell(region_id);
-CREATE INDEX idx_real_estate_sell__tn ON real_estate_sell(target_name);
-CREATE INDEX idx_real_estate_sell__cd ON real_estate_sell(contract_date);
-CREATE INDEX idx_real_estate_sell__ht ON real_estate_sell(housing_type);
-CREATE INDEX idx_real_estate_sell__p ON real_estate_sell(price);
-CREATE INDEX idx_real_estate_sell__nla ON real_estate_sell(net_leasable_area);
 -- -----------------------------------------------------------------------------------------
 
--- lease
+-- real_estate_lease
 -- -----------------------------------------------------------------------------------------
 -- create
+CREATE TABLE real_estate_lease
+(
+    id                      BIGINT  AUTO_INCREMENT PRIMARY KEY COMMENT '임대차 실거래가 ID',
+    region_id               BIGINT  NOT NULL COMMENT '대상지역 법정동 ID',
+    legal_dong_name         VARCHAR(50) COMMENT '법정동 명',
+    contract_date           DATE    COMMENT '계약일',
+    contract_start_month    DATE    COMMENT '계약시작 월',
+    contract_end_month      DATE    COMMENT '계약종료 월',
+    deposit                 INT     COMMENT '보증금',
+    monthly_rent            INT     COMMENT '월세',
+    previous_deposit        INT     COMMENT '이전 계약 보증금',
+    previous_monthly_rent   INT     COMMENT '이전 계약 월세',
+    build_year              INTEGER COMMENT '건축년도',
+    net_leasable_area       DECIMAL(10, 4) NOT NULL COMMENT '전용면적',
+    housing_type            ENUM('AP', 'MH', 'DT', 'MU', 'OF', 'O') NOT NULL DEFAULT 'O' COMMENT '주택유형',
+    target_name             VARCHAR(100) COMMENT '대상명',
+    floor                   INTEGER COMMENT '층',
+    created_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
+    updated_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시'
+) COMMENT = '부동산 임대차 실거래가';
 -- -----------------------------------------------------------------------------------------
 -- constraint
+ALTER TABLE real_estate_lease
+    ADD CONSTRAINT fk_real_estate_lease__region_id
+        FOREIGN KEY (region_id)
+            REFERENCES legal_dong(id)
+            ON UPDATE RESTRICT
+            ON DELETE RESTRICT;
 -- -----------------------------------------------------------------------------------------
 -- index
+CREATE INDEX idx_real_estate_lease__ri ON real_estate_lease(region_id);
 -- -----------------------------------------------------------------------------------------
 
 -- Spring Batch
