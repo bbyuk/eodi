c61d8b113d42272f2b06b64551acfcf10fdf79d2
bbyuk <k941026h@naver.com>
2025-09-22 15:15:18 +0900

    fix(eodi-batch): chunk ItemReader bean 등록시 jobExecutionContext 주입 위치 오류 수정

--- backend/eodi-batch/src/main/java/com/bb/eodi/batch/job/deal/load/config/MonthlyDealDataLoadChunkConfig.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/batch/job/deal/load/config/MonthlyDealDataLoadChunkConfig.java
@@ -19,89 +19,94 @@ import java.nio.file.Paths;
 @RequiredArgsConstructor
 public class MonthlyDealDataLoadChunkConfig {
 
-    // 임시파일 경로 jobContext 변수
-    @Value("#{jobExecutionContext['TEMP_FILE']}")
-    private String tempFilePath;
     private final ObjectMapper objectMapper;
     
     /**
      * 아파트 매매 데이터 적재 chunk ItemReader
+     * @param tempFilePath jobExecutionContext 값 임시 파일 경로
      * @return 아파트 매매 데이터 적재 chunk ItemReader
      */
     @Bean
     @StepScope
-    public ItemStreamReader<ApartmentSellDataItem> apartmentSellDataItemReader() {
+    public ItemStreamReader<ApartmentSellDataItem> apartmentSellDataItemReader(@Value("#{jobExecutionContext['TEMP_FILE']}") String tempFilePath) {
         return new RealEstateDealDataItemStreamReader<>(ApartmentSellDataItem.class, Paths.get(tempFilePath), objectMapper);
     }
 
     /**
      * 아파트 분양권/입주권 매매 데이터 적재 chunk ItemReader
+     * @param tempFilePath jobExecutionContext 값 임시 파일 경로
      * @return 아파트 분양권/입주권 매매 데이터 적재 chunk ItemReader
      */
     @Bean
     @StepScope
-    public ItemStreamReader<ApartmentPresaleRightSellDataItem> apartmentPresaleRightSellDataItemReader() {
+    public ItemStreamReader<ApartmentPresaleRightSellDataItem> apartmentPresaleRightSellDataItemReader(@Value("#{jobExecutionContext['TEMP_FILE']}") String tempFilePath) {
         return new RealEstateDealDataItemStreamReader<>(ApartmentPresaleRightSellDataItem.class, Paths.get(tempFilePath), objectMapper);
     }
 
     /**
      * 단독/다가구주택 매매 데이터 ItemStreamReader
+     * @param tempFilePath jobExecutionContext 값 임시 파일 경로
      * @return 단독/다가구주택 매매 데이터 ItemStreamReader
      */
     @Bean
     @StepScope
-    public ItemStreamReader<MultiUnitDetachedSellDataItem> multiUnitDetachedSellDataItemReader() {
+    public ItemStreamReader<MultiUnitDetachedSellDataItem> multiUnitDetachedSellDataItemReader(@Value("#{jobExecutionContext['TEMP_FILE']}") String tempFilePath) {
         return new RealEstateDealDataItemStreamReader<>(MultiUnitDetachedSellDataItem.class, Paths.get(tempFilePath), objectMapper);
     }
 
     /**
      * 연립/다세대주택 매매 데이터 적재 배치 chunk ItemReader
+     * @param tempFilePath jobExecutionContext 값 임시 파일 경로
      * @return 연립/다세대주택 매매 데이터 적재 배치 chunk ItemReader
      */
     @Bean
     @StepScope
-    public ItemStreamReader<MultiHouseholdHouseSellDataItem> multiHouseholdSellDataItemReader() {
+    public ItemStreamReader<MultiHouseholdHouseSellDataItem> multiHouseholdSellDataItemReader(@Value("#{jobExecutionContext['TEMP_FILE']}") String tempFilePath) {
         return new RealEstateDealDataItemStreamReader<>(MultiHouseholdHouseSellDataItem.class, Paths.get(tempFilePath), objectMapper);
     }
 
     /**
      * 오피스텔 매매 실거래가 데이터 적재 배치 chunk ItemReader
+     * @param tempFilePath jobExecutionContext 값 임시 파일 경로
      * @return 오피스텔 매매 실거래가 데이터 적재 배치 chunk ItemReader
      */
     @Bean
     @StepScope
-    public ItemStreamReader<OfficetelSellDataItem> officetelSellDataItemReader() {
+    public ItemStreamReader<OfficetelSellDataItem> officetelSellDataItemReader(@Value("#{jobExecutionContext['TEMP_FILE']}") String tempFilePath) {
         return new RealEstateDealDataItemStreamReader<>(OfficetelSellDataItem.class, Paths.get(tempFilePath), objectMapper);
     }
 
     /**
      * 아파트 임대차 실거래가 데이터 적재 배치 chunk step ItemReader
+     * @param tempFilePath jobExecutionContext 값 임시 파일 경로
      * @return 아파트 임대차 실거래가 데이터 적재 배치 chunk step ItemReader
      */
     @Bean
     @StepScope
-    public ItemStreamReader<ApartmentLeaseDataItem> apartmentLeaseDataItemReader() {
+    public ItemStreamReader<ApartmentLeaseDataItem> apartmentLeaseDataItemReader(@Value("#{jobExecutionContext['TEMP_FILE']}") String tempFilePath) {
         return new RealEstateDealDataItemStreamReader<>(ApartmentLeaseDataItem.class, Paths.get(tempFilePath), objectMapper);
     }
 
 
     /**
      * 단독/다가구주택 임대차 실거래가 데이터 적재 배치 chunk step ItemReader
+     * @param tempFilePath jobExecutionContext 값 임시 파일 경로
      * @return 단독/다가구주택 임대차 실거래가 데이터 적재 배치 chunk step ItemReader
      */
     @Bean
     @StepScope
-    public ItemStreamReader<MultiUnitDetachedLeaseDataItem> multiUnitDetachedLeaseDataItemReader() {
+    public ItemStreamReader<MultiUnitDetachedLeaseDataItem> multiUnitDetachedLeaseDataItemReader(@Value("#{jobExecutionContext['TEMP_FILE']}") String tempFilePath) {
         return new RealEstateDealDataItemStreamReader<>(MultiUnitDetachedLeaseDataItem.class, Paths.get(tempFilePath), objectMapper);
     }
 
     /**
      * 연립/다세대주택 임대차 실거래가 데이터 적재 배치 chunk step ItemReader
+     * @param tempFilePath jobExecutionContext 값 임시 파일 경로
      * @return 연립/다세대주택 임대차 실거래가 데이터 적재 배치 chunk step ItemReader
      */
     @Bean
     @StepScope
-    public ItemStreamReader<MultiHouseholdHouseLeaseDataItem> multiHouseholdHouseLeaseDataItemReader() {
+    public ItemStreamReader<MultiHouseholdHouseLeaseDataItem> multiHouseholdHouseLeaseDataItemReader(@Value("#{jobExecutionContext['TEMP_FILE']}") String tempFilePath) {
         return new RealEstateDealDataItemStreamReader<>(MultiHouseholdHouseLeaseDataItem.class, Paths.get(tempFilePath), objectMapper);
     }
 }
