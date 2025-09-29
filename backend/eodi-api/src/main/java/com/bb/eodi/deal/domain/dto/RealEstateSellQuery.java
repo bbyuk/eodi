cf87ea12cdc99e70f667ce397f463850b2571c69
bbyuk <k941026h@naver.com>
2025-09-29 15:37:02 +0900

    feat(eodi-api): 부동산 매매 실거래가 데이터 조회 API 파라미터 추가 및 미사용 파라미터 제거

--- backend/eodi-api/src/main/java/com/bb/eodi/deal/domain/dto/RealEstateSellQuery.java
+++ backend/eodi-api/src/main/java/com/bb/eodi/deal/domain/dto/RealEstateSellQuery.java
@@ -4,17 +4,25 @@ import com.bb.eodi.deal.domain.type.HousingType;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.YearMonth;
+import java.util.List;
+
 /**
  * 부동산 매매 실거래가 데이터 조회 query parameters
  */
 @Data
 @Builder
 public class RealEstateSellQuery {
-    // 가격 (만원단위)
-    private int maxPrice;
-    private int minPrice;
-    // 층
-    private Integer floor;
-    // 주택유형
-    private HousingType housingType;
+    // 최대 가격
+    private Integer maxPrice;
+    // 최소 가격
+    private Integer minPrice;
+    // 조회기간 시작월
+    private YearMonth startYearMonth;
+    // 조회기간 종료월
+    private YearMonth endYearMonth;
+    // 대상 지역 ID 리스트
+    private List<Long> targetRegionIds;
+    // 대상 주택유형 리스트
+    private List<HousingType> targetHousingTypes;
 }
--- backend/eodi-api/src/main/java/com/bb/eodi/deal/presentation/request/RealEstateSellRequestParameter.java
+++ backend/eodi-api/src/main/java/com/bb/eodi/deal/presentation/request/RealEstateSellRequestParameter.java
@@ -2,14 +2,24 @@ package com.bb.eodi.deal.presentation.request;
 
 import com.bb.eodi.deal.domain.type.HousingType;
 
+import java.time.YearMonth;
+import java.util.List;
+
 /**
  * 부동산 매매 실거래가 데이터 요청 파라미터
  */
 public record RealEstateSellRequestParameter(
-        //
-        int maxPrice,
-        int minPrice,
-        Integer floor,
-        HousingType housingType
+        // 최대 가격
+        Integer maxPrice,
+        // 최소 가격
+        Integer minPrice,
+        // 조회기간 시작월
+        YearMonth startYearMonth,
+        // 조회기간 종료월
+        YearMonth endYearMonth,
+        // 대상 지역 ID 리스트
+        List<Long> targetRegionIds,
+        // 대상 주택유형 리스트
+        List<HousingType> targetHousingTypes
 ) {
 }
