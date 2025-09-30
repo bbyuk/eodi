5d0d8cd187c5bfe81e5f563033b953b06ec61f2e
bbyuk <k941026h@naver.com>
2025-09-30 13:50:22 +0900

    hotfix/feat: 파일 생성 전처리 tasklet 로직 슈도코드 작성

--- backend/eodi-batch/src/main/java/com/bb/eodi/batch/job/deal/load/tasklet/RealEstateDealApiFetchStepTasklet.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/batch/job/deal/load/tasklet/RealEstateDealApiFetchStepTasklet.java
@@ -41,109 +41,15 @@ public class RealEstateDealApiFetchStepTasklet<T> implements Tasklet {
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         ExecutionContext jobCtx = contribution.getStepExecution().getJobExecution().getExecutionContext();
 
-        // 이전에 생성되어 있는 파일이 있는지 확인 후 API 요청 스킵여부 결정
-        if (jobCtx.containsKey(TEMP_FILE.name())) {
-            if (Files.exists(Paths.get(jobCtx.getString(TEMP_FILE.name())))) {
-                log.info("이전 실행에서 생성된 임시파일 발견. API 요청 skip. file={}", jobCtx.getString(TEMP_FILE.name()));
-                return RepeatStatus.FINISHED;
-            }
-            else {
-                log.warn("JobExecutionContext에는 경로가 있으나 실제 파일 없음. 새로 생성");
-            }
-        }
-
-
-        String dealMonth = jobCtx.getString(DEAL_MONTH.name());
-
-        // API 요청 대상 법정동 목록 조회
-        List<MonthlyLoadTargetLegalDongDto> monthlyLoadTargetLegalDongList = legalDongRepository.findAllSummary()
-                .stream()
-                .map(legalDongSummaryView -> new MonthlyLoadTargetLegalDongDto(
-                        legalDongSummaryView.sidoCode() + legalDongSummaryView.sigunguCode()))
-                .collect(Collectors.toList());
-
-        // temp file 생성
-        Path tempFilePath = Files.createTempFile(TEMP_FILE.name(), null);
-
         /**
-         * 전국 대상 API 요청 후 파일에 작성
-         * 실패시 생성된 임시 파일 삭제
+         * 1. jobExecutionContext parameter로 아래 항목 받아옴
+         *      1.1. 대상 주택유형
+         *      1.2. yearMonth
+         * 2. 파라미터 조합해 대상 파일 경로 get
+         * 3. jobExecutionContext에 TEMP_FILE.name() key로 setString()
          */
-        try (BufferedWriter bw = Files.newBufferedWriter(tempFilePath, StandardOpenOption.APPEND)) {
-            for (MonthlyLoadTargetLegalDongDto monthlyLoadTargetLegalDongDto : monthlyLoadTargetLegalDongList) {
-                /**
-                 * fetch를 1회 실행 해 totalCount 가져오기
-                 *
-                 */
-                DealDataResponse<T> countResponse = fetchByType(
-                        new DealDataQuery(
-                                monthlyLoadTargetLegalDongDto.regionCode(),
-                                dealMonth,
-                                0,
-                                0
-                        )
-                );
-                int totalCount = countResponse.body().getTotalCount();
-                int maxPageNum = (totalCount / pageSize) + 1;
-
-                log.debug("totalRowCount : {} at region code : {}", totalCount, monthlyLoadTargetLegalDongDto.regionCode());
-                log.debug("maxPageNum:{}", maxPageNum);
-
-                for (int pageNum = 1; pageNum <= maxPageNum; pageNum++) {
-                    log.debug("pageNum : {}", pageNum);
-                    DealDataResponse<T> apiResponse = fetchByType(
-                            new DealDataQuery(
-                                    monthlyLoadTargetLegalDongDto.regionCode(),
-                                    dealMonth,
-                                    pageSize,
-                                    pageNum)
-                    );
-                    List<T> apartmentSellDataItems = apiResponse.body().getItems();
-
-                    for (Object item : apartmentSellDataItems) {
-                        bw.write(objectMapper.writeValueAsString(item));
-                        bw.newLine();
-                    }
-                }
-            }
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            Files.delete(tempFilePath);
-            log.info("ApartmentSaleApiFetchStepTasklet -> temp file deleted. file={}", tempFilePath);
-            throw new RuntimeException(e);
-        }
-
-        /**
-         * 임시 파일명 jobContext에 저장
-         */
-        jobCtx.putString(TEMP_FILE.name(), tempFilePath.toString());
 
 
         return RepeatStatus.FINISHED;
-    }
-
-    @SuppressWarnings("unchecked")
-    private DealDataResponse<T> fetchByType(DealDataQuery query) {
-        if (targetClass.equals(ApartmentLeaseDataItem.class)) {
-            return (DealDataResponse<T>) dealDataApiClient.getApartmentLeaseData(query);
-        } else if (targetClass.equals(ApartmentPresaleRightSellDataItem.class)) {
-            return (DealDataResponse<T>) dealDataApiClient.getApartmentPresaleRightSellData(query);
-        } else if (targetClass.equals(ApartmentSellDataItem.class)) {
-            return (DealDataResponse<T>) dealDataApiClient.getApartmentSellData(query);
-        } else if (targetClass.equals(MultiHouseholdHouseLeaseDataItem.class)) {
-            return (DealDataResponse<T>) dealDataApiClient.getMultiHouseholdLeaseData(query);
-        } else if (targetClass.equals(MultiHouseholdHouseSellDataItem.class)) {
-            return (DealDataResponse<T>) dealDataApiClient.getMultiHouseholdSellData(query);
-        } else if (targetClass.equals(MultiUnitDetachedLeaseDataItem.class)) {
-            return (DealDataResponse<T>) dealDataApiClient.getMultiUnitDetachedLeaseData(query);
-        } else if (targetClass.equals(MultiUnitDetachedSellDataItem.class)) {
-            return (DealDataResponse<T>) dealDataApiClient.getMultiUnitDetachedSellData(query);
-        } else if (targetClass.equals(OfficetelLeaseDataItem.class)) {
-            return (DealDataResponse<T>) dealDataApiClient.getOfficetelLeaseData(query);
-        } else if (targetClass.equals(OfficetelSellDataItem.class)) {
-            return (DealDataResponse<T>) dealDataApiClient.getOfficetelSellData(query);
-        }
-
-        throw new RuntimeException("Unsupported target class: " + targetClass);
     }
 }
