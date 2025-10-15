8698fef9b4f0ed3dda59f0d71fbe7b021d2346ca
bbyuk <k941026h@naver.com>
2025-10-15 14:14:31 +0900

    chore(eodi-web): next.js ts -> js 설정 변경 / prettier 설정 추가

---
+++ frontend/eodi-web/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    /* config options here */
+    eslint: {
+        ignoreDuringBuilds: true, // 빌드 시 lint 무시
+    },
+};
+
+module.exports = nextConfig;
