98e49d1a5578ea7cc9ef212397646d22ab69f28f
hyuk <k941026h@naver.com>
2025-10-17 00:08:05 +0900

    db: legal_dong_adjacency 테이블 (source_id, target_id) unique 제약조건 추가

--- backend/db/schema/legal_dong/legal_dong_adjacency.sql
+++ backend/db/schema/legal_dong/legal_dong_adjacency.sql
@@ -11,6 +11,9 @@ CREATE TABLE legal_dong_adjacency
     updated_at  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시'
 )   COMMENT = '법정동 인접 정보';
 -- -----------------------------------------------------------------------------------------
+-- constraint
+ALTER TABLE legal_dong_adjacency
+ADD CONSTRAINT uk_source_target UNIQUE (source_id, target_id);
 
 -- index
 CREATE INDEX idx_legal_dong_adjacency__source_id ON legal_dong_adjacency(source_id);
