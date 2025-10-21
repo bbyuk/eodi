d3fe175f0569ac10c4ad99ab84b37b54dda8d8b5
bbyuk <k941026h@naver.com>
2025-10-21 09:09:58 +0900

    refactor(eodi-web): search 페이지 서버 컴포넌트로 변경 및 Wizard 구조 리팩토링

--- frontend/eodi-web/app/search/page.jsx
+++ frontend/eodi-web/app/search/page.jsx
@@ -1,23 +1,9 @@
-"use client";
 import Wizard from "@/components/flow/Wizard";
-import StepIndicator from "@/components/flow/StepIndicator";
-import { useState } from "react";
 
 export default function MainSearchPage() {
-  const [step, setStep] = useState(1);
-  const [prevStep, setPrevStep] = useState(1);
-
-  const goToStep = (next) => {
-    setPrevStep(step);
-    setStep(next);
-  };
-
   return (
     <div className="pt-24 w-full min-h-[100vh]">
-      <div className="flex justify-center mb-10">
-        <StepIndicator step={step} prevStep={prevStep} />
-      </div>
-      <Wizard step={step} prevStep={prevStep} goToStep={goToStep} />
+      <Wizard />
     </div>
   );
 }
