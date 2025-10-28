0a00bd7c19012d1af4a8d9ea324fba946a925e61
bbyuk <k941026h@naver.com>
2025-10-28 13:08:11 +0900

    chore(eodi-api): WebConfig 추가 / Cors 임시 전체 허용

---
+++ backend/eodi-api/src/main/java/com/bb/eodi/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.bb.eodi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * WebConfig
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
