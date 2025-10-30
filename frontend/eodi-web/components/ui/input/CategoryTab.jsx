c58ac1f08a2e4c88d7aa936e64a95c0973086078
hyuk <k941026h@naver.com>
2025-10-30 18:23:28 +0900

    feat(eodi-web): 카테고리 탭 버튼 type 분리

--- frontend/eodi-web/components/ui/input/CategoryTab.jsx
+++ frontend/eodi-web/components/ui/input/CategoryTab.jsx
@@ -1,10 +1,22 @@
 import CategoryButton from "@/components/ui/input/CategoryButton";
 
-export default function CategoryTab({ list, value, onSelect }) {
+/**
+ * 카테고리 탭 UI 컴포넌트
+ * @param list 카테고리 목록 list
+ * @param value 선택된 값
+ * @param onSelect 선택 이벤트 핸들러
+ * @param type 카테고리 탭 타입
+ */
+export default function CategoryTab({ list, value, onSelect, type = "toggle" }) {
   return (
     <div className="flex flex-wrap gap-2 mb-5">
       {list.map((data) => {
-        const isActive = value === data.code;
+        const isActive =
+          type === "toggle"
+            ? value === data.code
+            : type === "select"
+              ? value.has(data.code)
+              : false;
         return (
           <CategoryButton
             key={data.code}
