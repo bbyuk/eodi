0aaa136c1bf3e5a0a0bc88d72cd3bef23a902525
hyuk <k941026h@naver.com>
2025-10-30 17:24:51 +0900

    refactor(eodi-web): CategoryButton code 기반으로 변경

--- frontend/eodi-web/components/ui/input/CategoryTab.jsx
+++ frontend/eodi-web/components/ui/input/CategoryTab.jsx
@@ -4,12 +4,12 @@ export default function CategoryTab({ list, value, onSelect }) {
   return (
     <div className="flex flex-wrap gap-2 mb-5">
       {list.map((data) => {
-        const isActive = value === data;
+        const isActive = value === data.code;
         return (
           <CategoryButton
             key={data.code}
             icon={data.icon}
-            onClick={() => onSelect(data)}
+            onClick={() => onSelect(data.code)}
             isActive={isActive}
             label={data.displayName}
           />
