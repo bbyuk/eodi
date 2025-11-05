c063f0859fc82fbb6cc016cc8f100398f98d87ba
hyuk <k941026h@naver.com>
2025-11-06 00:08:38 +0900

    feat(eodi-web): step3 페이지 컴포넌트 변경

--- frontend/eodi-web/app/search/step3/page.jsx
+++ frontend/eodi-web/app/search/step3/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import OptionalFilters from "@/app/search/step3/OptionalFilters";
 import { motion } from "framer-motion";
 import { useSearchStore } from "@/app/search/store/searchStore";
 import { animation } from "@/app/search/_const/animation";
 import { direction } from "@/app/search/_const/direction";
+import DealListPage from "@/app/search/step3/DealListPage";
 
 export default function Step3Page() {
   const { currentDirection } = useSearchStore();
@@ -13,7 +13,7 @@ export default function Step3Page() {
       {...animation[direction[currentDirection].animation]}
       className="w-full px-8 pt-[12vh] md:pt-[18vh] pb-[8vh] overflow-x-hidden"
     >
-      <OptionalFilters />{" "}
+      <DealListPage />
     </motion.div>
   );
 }
