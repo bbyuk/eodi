9b9b6347fa36dba623398bae910b5d67e7a57b88
bbyuk <k941026h@naver.com>
2025-11-07 15:02:17 +0900

    fix(eodi-web): formatWon 메소드 comma 추가 로직 적용

--- frontend/eodi-web/app/search/_util/util.js
+++ frontend/eodi-web/app/search/_util/util.js
@@ -5,11 +5,21 @@ export const formatWon = (value) => {
   const eok = Math.floor((num % 100000000) / 10000);
   const man = num % 10000;
 
+  let eokStr = new String(eok);
+  let manStr = new String(man);
+
+  if (eok >= 1000) {
+    eokStr = `${eokStr.charAt(0)},${eokStr.substring(1, 4)}`;
+  }
+  if (man >= 1000) {
+    manStr = `${manStr.charAt(0)},${manStr.substring(1, 4)}`;
+  }
+
   let result = "";
 
   result += jo > 0 ? `${jo}조 ` : "";
-  result += eok > 0 ? `${eok}억 ` : "";
-  result += man > 0 ? `${man}만 ` : "";
+  result += eok > 0 ? `${eokStr}억 ` : "";
+  result += man > 0 ? `${manStr}만 ` : "";
   result += jo === 0 && eok === 0 && man === 0 ? "0 " : "";
 
   result += "원";
