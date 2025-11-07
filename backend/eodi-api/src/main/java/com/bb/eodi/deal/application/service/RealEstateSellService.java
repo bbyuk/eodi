26d08e12761c36dd79d29f993f04c11bfba92582
bbyuk <k941026h@naver.com>
2025-11-07 18:10:53 +0900

    refactor(eodi-api): 부동산 매매, 임대차 조회 일반 API requestParameter 레이어 변경에 따른 리팩토링작업 / domain layer 직접 참조 제거

--- backend/eodi-api/src/main/java/com/bb/eodi/deal/application/service/RealEstateSellService.java
+++ backend/eodi-api/src/main/java/com/bb/eodi/deal/application/service/RealEstateSellService.java
@@ -2,10 +2,12 @@ package com.bb.eodi.deal.application.service;
 
 import com.bb.eodi.deal.application.dto.RealEstateSellSummaryDto;
 import com.bb.eodi.deal.application.dto.RealEstateSellSummaryDtoMapper;
+import com.bb.eodi.deal.application.dto.request.RealEstateSellRequestParameter;
 import com.bb.eodi.deal.domain.dto.RealEstateSellQuery;
 import com.bb.eodi.deal.domain.repository.RealEstateSellRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -28,12 +30,26 @@ public class RealEstateSellService {
 
     /**
      * 부동산 매재 데이터 조회
-     * @param query 부동산 매매 데이터 조회 쿼리 파라미터
+     * @param requestParameter 부동산 매매 데이터 조회 요청 파라미터
+     * @param pageable pageable 객체
      * @return 부동산 매매 데이터 목록
      */
     @Transactional
-    public Page<RealEstateSellSummaryDto> findRealEstateSells(RealEstateSellQuery query, Pageable pageable) {
-        return realEstateSellRepository.findBy(query, pageable)
+    public Page<RealEstateSellSummaryDto> findRealEstateSells(
+            @ParameterObject RealEstateSellRequestParameter requestParameter,
+            @ParameterObject Pageable pageable) {
+        return realEstateSellRepository.findBy(
+                RealEstateSellQuery
+                        .builder()
+                        .maxPrice(requestParameter.maxPrice())
+                        .minPrice(requestParameter.minPrice())
+                        .maxNetLeasableArea(requestParameter.maxNetLeasableArea())
+                        .minNetLeasableArea(requestParameter.minNetLeasableArea())
+                        .startYearMonth(requestParameter.startYearMonth())
+                        .endYearMonth(requestParameter.endYearMonth())
+                        .targetHousingTypes(requestParameter.targetHousingTypes())
+                        .targetRegionIds(requestParameter.targetRegionIds())
+                        .build(), pageable)
                 .map(summaryDtoMapper::toDto);
     }
 
