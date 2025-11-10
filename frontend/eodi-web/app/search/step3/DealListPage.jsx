60225f129443f7aa104e130750ffea652e3c4b06
bbyuk <k941026h@naver.com>
2025-11-10 15:35:59 +0900

    feat(eodi-web): result page 매매/임대차 탭 버튼 추가

--- frontend/eodi-web/app/search/step3/DealListPage.jsx
+++ frontend/eodi-web/app/search/step3/DealListPage.jsx
@@ -11,6 +11,8 @@ import ResultGrid from "@/app/search/step3/_components/ResultGrid";
 import ResultCard from "@/app/search/step3/_components/ResultCard";
 import { api } from "@/lib/apiClient";
 import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
+import GridGroup from "@/app/search/_components/GridGroup";
+import CategoryTab from "@/components/ui/input/CategoryTab";
 
 // url: "https://new.land.naver.com/complexes?ms=노원구 중계동 아파트",
 const id = "result";
@@ -19,6 +21,16 @@ export default function DealListPage() {
   const title = "선택한 지역의 실거래 내역을 찾았어요";
   const description = ["최근 3개월간의 실거래 데이터를 기준으로 표시됩니다."];
   const pageSize = 30;
+  const tabList = [
+    {
+      code: "sell",
+      displayName: "매매",
+    },
+    {
+      code: "lease",
+      displayName: "임대차",
+    },
+  ];
 
   const [sellTotalCount, setSellTotalCount] = useState(0);
   const [sellTotalPages, setSellTotalPages] = useState(0);
@@ -35,6 +47,7 @@ export default function DealListPage() {
   } = useSearchStore();
 
   const [isFloatingCardOpen, setIsFloatingCardOpen] = useState(false);
+  const [selectedTab, setSelectedTab] = useState(tabList[0].code);
 
   const findSell = () => {
     if (isSellLoading) {
@@ -62,7 +75,6 @@ export default function DealListPage() {
       })
       .finally(() => setIsSellLoading(false));
   };
-
   useEffect(() => {
     setCurrentContext(context[id]);
     findSell();
@@ -87,13 +99,16 @@ export default function DealListPage() {
       {/* Header */}
       <PageHeader title={title} description={description} />
 
-      {/* Grid */}
-      <ResultGrid>
-        {sellList.map((sell) => (
-          <ResultCard key={sell.id} data={sell} dealType={"매매"} />
-        ))}
-        <div ref={loadMoreRef} className={"h-6"} />
-      </ResultGrid>
+      <GridGroup>
+        <CategoryTab list={tabList} type={"toggle"} value={selectedTab} onSelect={setSelectedTab} />
+        {/* Grid */}
+        <ResultGrid>
+          {sellList.map((sell) => (
+            <ResultCard key={sell.id} data={sell} dealType={"매매"} />
+          ))}
+          <div ref={loadMoreRef} className={"h-6"} />
+        </ResultGrid>
+      </GridGroup>
     </main>
   );
 }
