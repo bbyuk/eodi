7993a1b35d0462ffce0446952bcbfa98aa49be49
hyuk <k941026h@naver.com>
2025-11-13 00:52:11 +0900

    feat(eodi-batch): 도로명 주소 적재 배치 jobConfig 초안 작성 / 미사용 Tasklet 제거, ItemReader, RoadNameAddress entity 추가

---
+++ backend/eodi-batch/src/main/java/com/bb/eodi/batch/job/address/roadname/config/RoadNameAddressLoadJobConfig.java
@@ -0,0 +1,119 @@
+package com.bb.eodi.batch.job.address.roadname.config;
+
+import com.bb.eodi.batch.core.config.EodiBatchProperties;
+import com.bb.eodi.batch.job.address.roadname.dto.RoadNameAddressItem;
+import com.bb.eodi.batch.job.address.roadname.entity.RoadNameAddress;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.MultiResourceItemReader;
+import org.springframework.batch.item.file.ResourceAwareItemReaderItemStream;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Objects;
+
+/**
+ * 도로명 주소 적재 배치 jobConfig
+ */
+@Configuration
+@RequiredArgsConstructor
+public class RoadNameAddressLoadJobConfig {
+
+    private final JobRepository jobRepository;
+    private final EodiBatchProperties eodiBatchProperties;
+    private final PlatformTransactionManager transactionManager;
+
+    /**
+     * 도로명주소 적재 job
+     *
+     * 도로명주소 전체분 파일을 일괄 load한다.
+     * DB초기 구축시 사용
+     *
+     * @param roadNameAddressLoadStep 도로명주소 데이터를 로드해 DB에 적재하는 Step
+     * @return 도로명주소 적재 job
+     */
+    @Bean
+    public Job roadNameAddressLoadJob(Step roadNameAddressLoadStep) {
+        return new JobBuilder("roadNameAddressLoadJob", jobRepository)
+                .start(roadNameAddressLoadStep)
+                .build();
+    }
+
+    @Bean
+    public Step roadNameAddressLoadStep(
+            ItemReader<RoadNameAddressItem> multiResourceRoadNameAddressItemReader,
+            ItemProcessor<RoadNameAddressItem, RoadNameAddress> roadNameAddressItemProcessor,
+            ItemWriter<RoadNameAddress> roadNameAddressItemWriter
+    ) {
+        return new StepBuilder("roadNameAddressLoadStep", jobRepository)
+                .<RoadNameAddressItem, RoadNameAddress>chunk(eodiBatchProperties.batchSize(), transactionManager)
+                .reader(multiResourceRoadNameAddressItemReader)
+                .processor(roadNameAddressItemProcessor)
+                .writer(roadNameAddressItemWriter)
+                .build();
+    }
+
+    /**
+     * 도로명주소 멀티 리소스 reader
+     * @param targetDirectoryPath 대상 directory 경로
+     * @param roadNameAddressItemReader 단일 resource ItemReader
+     * @return 도로명주소 멀티 리소스 reader
+     */
+    @Bean
+    @StepScope
+    public MultiResourceItemReader<RoadNameAddressItem> multiResourceRoadNameAddressItemReader(
+            @Value("#{jobParameters['target-directory']}") String targetDirectoryPath,
+            ResourceAwareItemReaderItemStream<RoadNameAddressItem> roadNameAddressItemReader
+    ) {
+        MultiResourceItemReader<RoadNameAddressItem> reader = new MultiResourceItemReader<>();
+
+        File directory = new File(targetDirectoryPath);
+        Resource[] roadNameAddressFileResources = Arrays.stream(Objects.requireNonNull(directory.listFiles()))
+                .filter(file -> file.getName().startsWith("road"))
+                .map(FileSystemResource::new)
+                .toArray(Resource[]::new);
+
+        reader.setResources(roadNameAddressFileResources);
+        reader.setDelegate(roadNameAddressItemReader);
+
+        return reader;
+    }
+
+
+    /**
+     * 도로명주소 ItemProcessor
+     * TODO ItemProcessor 개발 및 파일 분리
+     * @return 도로명주소 ItemProcessor
+     */
+    @Bean
+    @StepScope
+    public ItemProcessor<RoadNameAddressItem, RoadNameAddress> roadNameAddressItemProcessor() {
+        return item -> null;
+    }
+
+    /**
+     * 도로명주소 ItemWriter
+     * TODO ItemWriter 개발 및 파일 분리
+     * @return 도로명주소 ItemWriter
+     */
+    @Bean
+    @StepScope
+    public ItemWriter<RoadNameAddress> roadNameAddressItemWriter() {
+        return item -> {};
+    }
+
+}
---
+++ backend/eodi-batch/src/main/java/com/bb/eodi/batch/job/address/roadname/entity/RoadNameAddress.java
@@ -0,0 +1,12 @@
+package com.bb.eodi.batch.job.address.roadname.entity;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * 도로명 주소 address 도메인 entity
+ */
+@Data
+@Builder
+public class RoadNameAddress {
+}
