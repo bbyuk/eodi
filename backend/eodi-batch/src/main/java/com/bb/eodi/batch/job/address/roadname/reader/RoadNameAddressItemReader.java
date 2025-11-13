d322e3082ca036787c4dfb394cf315d69ca6d3a2
hyuk <k941026h@naver.com>
2025-11-13 22:44:03 +0900

    fix(eodi-batch): 도로명 주소 ItemReader 에러 로그 추가 / 말소일자 예외 처리

--- backend/eodi-batch/src/main/java/com/bb/eodi/batch/job/address/roadname/reader/RoadNameAddressItemReader.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/batch/job/address/roadname/reader/RoadNameAddressItemReader.java
@@ -73,32 +73,39 @@ public class RoadNameAddressItemReader implements ResourceAwareItemReaderItemStr
 
         readCounter++;
 
-        String[] split = line.split("\\|");
+        String[] split = line.split("|");
 
-        return RoadNameAddressItem
-                .builder()
-                .sigunguCode(split[0])
-                .roadNameNo(split[1])
-                .roadNameCode(split[0] + split[1])
-                .roadName(split[2])
-                .engRoadName(split[3])
-                .umdSeq(split[4])
-                .sidoName(split[5])
-                .sigunguName(split[6])
-                .umdGb(split[7])
-                .umdCode(split[8])
-                .umdName(split[9])
-                .parentRoadNameNo(split[10])
-                .parentRoadName(split[11])
-                .useYn(split[12])
-                .changeHistoryReason(split[13])
-                .changeHistoryInfo(split[14])
-                .engSidoName(split[15])
-                .engSigunguName(split[16])
-                .engUmdName(split[17])
-                .announcementDate(split[18])
-                .expirationDate(split[19])
-                .build();
+        try {
+            return RoadNameAddressItem
+                    .builder()
+                    .sigunguCode(split[0])
+                    .roadNameNo(split[1])
+                    .roadNameCode(split[0] + split[1])
+                    .roadName(split[2])
+                    .engRoadName(split[3])
+                    .umdSeq(split[4])
+                    .sidoName(split[5])
+                    .sigunguName(split[6])
+                    .umdGb(split[7])
+                    .umdCode(split[8])
+                    .umdName(split[9])
+                    .parentRoadNameNo(split[10])
+                    .parentRoadName(split[11])
+                    .useYn(split[12])
+                    .changeHistoryReason(split[13])
+                    .changeHistoryInfo(split[14])
+                    .engSidoName(split[15])
+                    .engSigunguName(split[16])
+                    .engUmdName(split[17])
+                    .announcementDate(split[18])
+                    .expirationDate(split.length == 20 ? split[19] : null)
+                    .build();
+        }
+        catch(ArrayIndexOutOfBoundsException e) {
+            log.error("매핑 실패 : {}", e.getMessage(), e);
+            log.error("line={}", line);
+            throw new ItemStreamException(e);
+        }
     }
 
     @Override
