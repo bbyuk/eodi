0d24b21b85df617284414f7a872e0d81b0829947
hyuk <k941026h@naver.com>
2025-11-20 23:23:46 +0900

    refactor(eodi-batch): 법정동 조회 API 명세 @HttpExchange 인터페이스로 분리

--- backend/eodi-batch/src/main/java/com/bb/eodi/integration/gov/legaldong/LegalDongDataApi.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/integration/gov/legaldong/LegalDongDataApi.java
@@ -1,7 +1,35 @@
 package com.bb.eodi.integration.gov.legaldong;
 
+import com.bb.eodi.integration.gov.legaldong.dto.LegalDongApiResponse;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
+
 /**
  * 법정동 데이터 외부 API 스펙
  */
+@HttpExchange(
+        url = "1741000",
+        accept = "application/json",
+        contentType = "application/json"
+)
 public interface LegalDongDataApi {
+
+    /**
+     * 지역별 법정동 조회 - 지역명 조회
+     * @param apiKey 공공데이터 포털 발급 API key
+     * @param pageNum 현재 페이지
+     * @param pageSize 페이지 크기
+     * @param targetRegion 대상 지역명
+     * @param type API type
+     * @return 법정동 조회 API 응답
+     */
+    @GetExchange("/StanReginCd/getStanReginCdList")
+    LegalDongApiResponse getLegalDong(
+            @RequestParam("serviceKey") String apiKey,
+            @RequestParam("pageNo") int pageNum,
+            @RequestParam("numOfRows") int pageSize,
+            @RequestParam("locatadd_nm") String targetRegion,
+            @RequestParam("type") String type
+    );
 }
