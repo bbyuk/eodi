1e9a356d78ad6fdd5ad414bd30f6da4da8e9d944
bbyuk <k941026h@naver.com>
2025-11-25 10:07:38 +0900

    feat(eodi-batch): 지번 dto 추가

---
+++ backend/eodi-batch/src/main/java/com/bb/eodi/deal/domain/dto/LandLot.java
@@ -0,0 +1,13 @@
+package com.bb.eodi.deal.domain.dto;
+
+import lombok.Data;
+
+/**
+ * 지번
+ */
+@Data
+public class LandLot {
+    private Integer landLotMainNo;
+    private Integer landLotSubNo;
+    private Boolean isMountain;
+}
