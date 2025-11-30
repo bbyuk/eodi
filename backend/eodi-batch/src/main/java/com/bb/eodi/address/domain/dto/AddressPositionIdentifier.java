b645bafffd2ae4d52044d1ea2a2c34b97f3d468e
hyuk <k941026h@naver.com>
2025-12-01 00:23:38 +0900

    feat(eodi-batch): 주소 위치정보 Identifier dto 추가

---
+++ backend/eodi-batch/src/main/java/com/bb/eodi/address/domain/dto/AddressPositionIdentifier.java
@@ -0,0 +1,37 @@
+package com.bb.eodi.address.domain.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Objects;
+
+/**
+ * 주소 위치 테이블 Identifier 도메인 PK dto
+ */
+@Data
+@Builder
+public class AddressPositionIdentifier {
+    private String roadNameCode;
+    private String legalDongCode;
+    private String isUnderground;
+    private Integer buildingMainNo;
+    private Integer buildingSubNo;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AddressPositionIdentifier)) return false;
+        AddressPositionIdentifier other = (AddressPositionIdentifier) o;
+
+        return Objects.equals(this.roadNameCode, other.getRoadNameCode())
+                && Objects.equals(this.legalDongCode, other.getLegalDongCode())
+                && Objects.equals(this.isUnderground, other.getIsUnderground())
+                && Objects.equals(this.buildingMainNo, other.getBuildingMainNo())
+                && Objects.equals(this.buildingSubNo, other.getBuildingSubNo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roadNameCode, legalDongCode, isUnderground, buildingMainNo, buildingSubNo);
+    }
+}
