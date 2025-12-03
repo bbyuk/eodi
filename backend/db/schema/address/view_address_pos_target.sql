64567c7ed4fd54bfa999cc61e64fbe9fb2e4587d
bbyuk <k941026h@naver.com>
2025-12-03 10:25:22 +0900

    db: 주소위치 대상 view ddl 오류 수정

--- backend/db/schema/address/view_address_pos_target.sql
+++ backend/db/schema/address/view_address_pos_target.sql
@@ -8,8 +8,14 @@ SELECT DISTINCT road_name_code,
 FROM building_address
 UNION
 SELECT DISTINCT
+    lla.road_name_code,
+    lla.legal_dong_code,
+    ba.is_underground,
+    ba.building_main_no,
+    ba.building_sub_no
 FROM building_address ba
          JOIN land_lot_address lla
-              ON ba.road_name_code = lla.road_name_code
-                  AND ba.is_underground = lla.is_underground
-                  AND ba.building_main_no = lla
+              ON  ba.road_name_code 	= lla.road_name_code
+                  AND ba.is_underground 	= lla.is_underground
+                  AND ba.building_main_no 	= lla.building_main_no
+                  AND ba.building_sub_no	= lla.building_sub_no;
