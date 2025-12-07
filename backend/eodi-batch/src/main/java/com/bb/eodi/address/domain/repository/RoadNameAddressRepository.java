631adcdb3bf5758c6fd101be4fc6d850e5bc004b
hyuk <k941026h@naver.com>
2025-12-07 16:19:04 +0900

    feat(eodi-batch): RoadNameAddressRepository 관리번호로 도로명주소 조회 메소드 추가

--- backend/eodi-batch/src/main/java/com/bb/eodi/address/domain/repository/RoadNameAddressRepository.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/address/domain/repository/RoadNameAddressRepository.java
@@ -3,6 +3,7 @@ package com.bb.eodi.address.domain.repository;
 import com.bb.eodi.address.domain.entity.RoadNameAddress;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 도로명주소 repository
@@ -14,4 +15,11 @@ public interface RoadNameAddressRepository {
      * @param items 배치 insert할 데이터
      */
     void insertBatch(List<? extends RoadNameAddress> items);
+
+    /**
+     * 주소 관리번호로 도로명주소를 검색한다.
+     * @param addressManageNo 주소 관리 번호
+     * @return 도로명 주소
+     */
+    Optional<RoadNameAddress> findByManageNo(String addressManageNo);
 }
