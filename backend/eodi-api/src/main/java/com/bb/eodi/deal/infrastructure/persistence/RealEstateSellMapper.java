99700cfc831f3fb2e39001d1cb9650a614969eab
bbyuk <k941026h@naver.com>
2025-12-18 16:15:15 +0900

    fix(eodi-batch): 부동산 매매, 임대차 실거래가 xPos, yPos Mapper 매핑 어노테이션 추가

--- backend/eodi-api/src/main/java/com/bb/eodi/deal/infrastructure/persistence/RealEstateSellMapper.java
+++ backend/eodi-api/src/main/java/com/bb/eodi/deal/infrastructure/persistence/RealEstateSellMapper.java
@@ -3,11 +3,17 @@ package com.bb.eodi.deal.infrastructure.persistence;
 import com.bb.eodi.deal.domain.entity.RealEstateSell;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 /**
  * 부동산 매매 실거래가 Jpa Entity - domain Entity mapper
  */
 @Mapper(componentModel = "spring")
 public interface RealEstateSellMapper {
+
+    @Mappings({
+            @Mapping(target = "xPos", source = "XPos"),
+            @Mapping(target = "yPos", source = "YPos")
+    })
     RealEstateSell toDomain(RealEstateSellJpaEntity entity);
 }
