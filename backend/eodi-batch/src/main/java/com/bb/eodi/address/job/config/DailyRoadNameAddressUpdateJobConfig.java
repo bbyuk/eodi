08f592ceeb10bc948ef73530318e8a33f4c59b31
bbyuk <k941026h@naver.com>
2026-01-15 12:54:46 +0900

    feat(eodi-batch): 기준정보 버전 확인 후 필요한 일변동파일 API를 통한 다운로드 tasklet 구현

--- backend/eodi-batch/src/main/java/com/bb/eodi/address/job/config/DailyRoadNameAddressUpdateJobConfig.java
+++ backend/eodi-batch/src/main/java/com/bb/eodi/address/job/config/DailyRoadNameAddressUpdateJobConfig.java
@@ -2,19 +2,30 @@ package com.bb.eodi.address.job.config;
 
 import com.bb.eodi.address.domain.port.AddressLinkageApiPort;
 import com.bb.eodi.core.EodiBatchProperties;
+import com.bb.eodi.ops.domain.entity.ReferenceVersion;
 import com.bb.eodi.ops.domain.repository.ReferenceVersionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobInterruptedException;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoUnit;
 
 /**
  * 도로명주소 일변동 적용 일배치 job config
@@ -67,12 +78,50 @@ public class DailyRoadNameAddressUpdateJobConfig {
             ReferenceVersionRepository referenceVersionRepository,
             AddressLinkageApiPort addressLinkageApiPort
     ) {
-        /**
-         * TODO
-         * 1. 기준정보 현재 적용 일자 조회
-         * 2. 주소 / 주소 입구 정보 적용 일자 가져와서 그 다음날부터 step 수행
-         * 3. 모두 처리 후 파일 삭제
-         */
-        return null;
+        return (contribution, chunkContext) -> {
+            ExecutionContext jobCtx = contribution.getStepExecution().getJobExecution().getExecutionContext();
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyMMdd");
+
+            /**
+             * TODO
+             * 1. 기준정보 현재 적용 일자 조회
+             * 2. 주소 / 주소 입구 정보 적용 일자 가져와서 그 다음날부터 step 수행
+             * 3. 모두 처리 후 파일 삭제 -> 처리 후 스텝에서 작업
+             */
+            String addressReferenceName = "address";
+            ReferenceVersion referenceVersion = referenceVersionRepository.findByTargetName(addressReferenceName)
+                    .orElseThrow(() -> new RuntimeException(addressReferenceName + " 기준정보 버전 정보를 찾지 못했습니다."));
+
+            LocalDate today = LocalDate.now();
+            if (!today.isAfter(referenceVersion.getEffectiveDate())) {
+                throw new JobInterruptedException("주소 DB가 이미 최신 상태입니다.");
+            }
+            LocalDate fromDate = referenceVersion.getEffectiveDate().plusDays(1);
+
+            long between = ChronoUnit.DAYS.between(fromDate, today);
+
+            /**
+             * 10일 초과시 10일씩 나눠서 api 요청 및 jobExecutionContext에 등록
+             */
+            if (between > 10) {
+                long mul = between / 10;
+
+                for (int i = 0; i < mul; i++) {
+                    LocalDate fixedFromDate = fromDate.plusDays(i * 10);
+                    LocalDate fixedToDate = fromDate.plusDays((i + 1) * 10);
+
+                    addressLinkageApiPort.downloadUpdatedAddress(targetDirectory, fixedFromDate, fixedToDate);
+                }
+
+                LocalDate lastFromDate = fromDate.plusDays(mul * 10);
+                addressLinkageApiPort.downloadUpdatedAddress(targetDirectory, lastFromDate, today);
+            }
+
+
+            jobCtx.put("fromDate", fromDate.format(dtf));
+            jobCtx.put("toDate", today.format(dtf));
+
+            return RepeatStatus.FINISHED;
+        };
     }
 }
